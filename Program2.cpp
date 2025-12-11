f7a5b4c2bf3029629d13d75650b2022b0974b852
amrutamanda22-coder <amrutamanda22@gmail.com>
2025-12-11 21:14:09 +0530

    Basics How to Print

---
+++ Program2.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout<<"Hello Amruta"; 
+    cout<<endl; //for new line
+    cout<<"Hello Vaishnavi";
+
+    return 0;
+}
---
+++ Program3.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    //Ways u can print new line
+    cout<<"Hello Amruta!";
+    cout<<endl;
+    cout<<"Hello Manda!";
+
+    cout<<"Hello Amruta!\n";
+    cout<<"Hello Manda!";
+
+    cout<<"Hello Amruta!";
+    cout<<"\n";
+    cout<<"Hello Manda!";
+
+    cout<<"Hello Amruta!"<<endl<<"Hello Manda!";
+    return 0;
+}
