2457a099c843d17f7365cacc7bc0b4de9e4ba4b2
amrutamanda22-coder <amrutamanda22@gmail.com>
2025-12-11 22:28:25 +0530

    Square of 2 numbers

---
+++ Program9.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    float n;
+
+    cout<<"Enter any number to find square of it:";
+    cin>>n;
+
+    cout<<"Square of a Number:"<<n*n<<endl;
+    return 0;
+}
