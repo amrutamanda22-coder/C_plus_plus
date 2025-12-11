899fa715d050e247d1855b869283139ab2b07e18
amrutamanda22-coder <amrutamanda22@gmail.com>
2025-12-11 21:16:43 +0530

    Increment and Decrement Operators

---
+++ Program5.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    // Increment and Decrement Operators
+    int x = 4;
+    cout<<x<<endl;
+    x++; //Post Increment
+    cout<<x<<endl;
+    //x = x + 1
+    //x += 1
+    //x++
+    x--; //Post Decrement
+    cout<<x<<endl;
+    ++x; //Pre Increment
+    cout<<x<<endl;
+    --x; //Pre Decrement
+    cout<<x<<endl;
+    return 0;
+}
