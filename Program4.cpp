fb47d4d0818344be32b9bfd95e4f1ae5b1718e6d
amrutamanda22-coder <amrutamanda22@gmail.com>
2025-12-11 21:16:01 +0530

    Variables and Arithmetic Operators

---
+++ Program4.cpp
@@ -0,0 +1,50 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout<<4<<"\n";
+    cout<<"4\n";
+    cout<<4+3<<"\n";
+    cout<<"4+3\n";
+
+    int x ; //declaration
+    x = 5; //intialization
+    int y = 9;
+
+    cout<<x<<"\n";
+    cout<<"x\n";
+    cout<<x+8<<"\n";
+    cout<<x+y<<"\n";
+    cout<<x*y<<"\n";
+
+    cout<<x<<endl;
+    x = 8;
+    cout<<x<<endl;
+    x = x + 2;
+    cout<<x<<endl;
+    x = x - 8;
+    cout<<x<<endl;
+    x = x * 10;
+    cout<<x<<endl;
+
+    //Arthemetic Operations
+
+    int a = 5,b = 4;
+
+    cout<<a + b<<endl;
+    cout<<a - b<<endl;
+    cout<<a * b<<endl;
+    cout<<a / b<<endl;
+    
+    float p = 7,q = 2;
+
+    cout<<p<<endl;
+    cout<<q<<endl;
+
+    cout<<p + q<<endl;
+    cout<<p - q<<endl;
+    cout<<p * q<<endl;
+    cout<<p / q<<endl;
+
+    return 0;
+}
