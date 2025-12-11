43d7010ecf7a4b7d13b948fbde1284ab741b63ea
amrutamanda22-coder <amrutamanda22@gmail.com>
2025-12-11 22:29:20 +0530

    Product of 3 numbers

---
+++ Program11.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    float a,b,c;
+
+    cout<<"Enter first number:";
+    cin>>a;
+
+    cout<<"ENter second number:";
+    cin>>b;
+
+    cout<<"Enter third number:";
+    cin>>c;
+
+    cout<<"Product of 3 numbers is "<<a*b*c;
+    
+    return 0;
+}
