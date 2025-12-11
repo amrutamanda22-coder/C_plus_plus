6ac97a55c8b20eaa0eed5f19debe5280fb5a5437
amrutamanda22-coder <amrutamanda22@gmail.com>
2025-12-11 22:29:00 +0530

    Sum of 2 numbers

---
+++ Program10.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    float x,y;
+
+    cout<<"Enter first number:";
+    cin>>x;
+
+    cout<<"Enter second number:";
+    cin>>y;
+
+    cout<<"Sum of Two Numbers:"<<x+y;
+
+    return 0;
+}
