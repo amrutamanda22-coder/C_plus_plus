463ffdef92f866b211f2aae7a56e689dfa808d57
amrutamanda22-coder <amrutamanda22@gmail.com>
2025-12-11 21:17:48 +0530

    Area of a Circle

---
+++ Program6.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    float r,a,PI = 3.141592;
+
+    cout<<"Enter Radius: ";
+    cin>>r;
+
+    a=PI * r * r;
+
+    cout<<"Area of Circle :"<<a<<endl;
+
+    return 0;
+}
