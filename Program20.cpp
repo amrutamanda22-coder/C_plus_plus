4621cf271cbb647c56d0e50a68bc212b118d1aa4
amrutamanda22-coder <amrutamanda22@gmail.com>
2025-12-12 21:10:37 +0530

    Checking whether the entered number is divisible by 5 and 3 or not

---
+++ Program20.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+
+    cout<<"Enter a number:";
+    cin>>n;
+
+    if(n%5==0 && n%3==0) //(n%15==0)
+    cout<<n<<" Is divisible by 3 and 5";
+
+    else
+    cout<<n<<" Is NOT divisible by 3 and 5";
+
+    return 0;
+}
