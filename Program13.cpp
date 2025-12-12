b4e4ef0ea47cc24d8292f08f7468207d385081fa
amrutamanda22-coder <amrutamanda22@gmail.com>
2025-12-12 13:34:56 +0530

    Typecasting using int and char datatypes

---
+++ Program13.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    //Typecasting (char to int)
+    char ch;
+    cin>>ch;
+    cout<<int(ch)<<endl;
+
+    //Typecasting (int to char)
+    int x;
+    cin>>x;
+    cout<<char(x);
+
+    return 0;
+}
