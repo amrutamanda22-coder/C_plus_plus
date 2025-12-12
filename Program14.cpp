b0668591962f146793155b31a9e988598e43f177
amrutamanda22-coder <amrutamanda22@gmail.com>
2025-12-12 13:47:57 +0530

    Float Arithmetic Operations

---
+++ Program14.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout<<5/2<<endl;
+    cout<<5.0/2<<endl;
+    cout<<5/2.0<<endl;
+    cout<<5.0/2.0<<endl;
+
+    float x = 7.0/22*(3.14+2)*3/5;
+    cout<<x;
+
+    return 0;
+}
