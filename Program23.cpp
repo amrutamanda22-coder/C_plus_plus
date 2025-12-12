37c7424097d3260614bc363cb650cab1634f72ac
amrutamanda22-coder <amrutamanda22@gmail.com>
2025-12-12 23:51:12 +0530

    Checking divisiblity of number by 5 or 3 but NOT 15

---
+++ Program23.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    
+    cout<<"Enter a number:";
+    cin>>n;
+
+    if((n%5==0 || n%3==0)&& n%15!=0)
+    cout<<n<<" is divisible by 5 or 3 but not 15";
+
+    else
+    cout<<n<<" is divisible by 5 or 3 and 15";
+
+
+    return 0;
+}
